67d00fcc5d47e27833f9dfa54210f7d148776236
shahadot99999 <shahadot99999@gmail.com>
2022-09-04 16:39:52 +0600

    again

--- js/app.js
+++ js/app.js
@@ -37,16 +37,3 @@ document.getElementById('btn-search').addEventListener('click', function () {
 
 loadTemperature('dhaka');
 
-// const setInnerText = (id, text) => {
-//     document.getElementById(id).innerText = text;
-// }
-
-// const displayTemperature = temperature => {
-//     setInnerText('city', temperature.name);
-//     setInnerText('temperature', temperature.main.temp);
-//     setInnerText('condition', temperature.weather[0].main);
-//     // set weather icon
-//     const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
-//     const imgIcon = document.getElementById('weather-icon');
-//     imgIcon.setAttribute('src', url);
-// }
